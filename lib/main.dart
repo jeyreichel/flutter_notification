fc90568ca28b37fcf32c3b1f801a8168a7dfed10
foxdev810 <jeyjy0810@gmail.com>
2024-05-16 22:38:54 +0800

    notification

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Scaffold demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GeeksforGeeks Scaffold'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open Snackbar'),
+          onPressed: () {
+            Get.snackbar(
+              "GeeksforGeeks",
+              "Hello everyone",
+              icon: Icon(Icons.person, color: Colors.white),
+              snackPosition: SnackPosition.BOTTOM,
+              backgroundColor: Colors.green,
+              borderRadius: 20,
+              margin: EdgeInsets.all(15),
+              colorText: Colors.white,
+              duration: Duration(seconds: 4),
+              isDismissible: true,
+              forwardAnimationCurve: Curves.easeOutBack,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
